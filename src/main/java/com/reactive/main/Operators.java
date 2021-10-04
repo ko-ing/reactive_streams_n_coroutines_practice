719a9975d5e706db32c9545dd223b79840f0efe1
ko-ing <318921@naver.com>
2021-10-04 14:37:26 +0900

    first commit

---
+++ src/main/java/com/reactive/main/Operators.java
@@ -0,0 +1,7 @@
+package com.reactive.main;
+
+public class Operators {
+    public static void main(String[] args) {
+        System.out.println("Hey There");
+    }
+}
---
+++ src/main/java/com/reactive/reactive_streams_practice/ReactiveStreamsPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.reactive.reactive_streams_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveStreamsPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveStreamsPracticeApplication.class, args);
+    }
+
+}
