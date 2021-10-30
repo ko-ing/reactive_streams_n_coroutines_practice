5b4150c596b2cbf0551dc49eeb16266491535d29
ko-ing <318921@naver.com>
2021-10-30 12:37:30 +0900

    [Flux] flux flux

--- src/main/java/com/reactive/flux/FluxApplication.java
+++ src/main/java/com/reactive/flux/FluxApplication.java
@@ -12,8 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 @Slf4j
@@ -34,12 +36,36 @@ public class FluxApplication {
         return Mono.just(list);
     }
 
+    // MediaType.TEXT_EVENT_STREAM_VALUE을 통해 스트림으로 데이터를 받을 수 있게 한다.
     @GetMapping(value = "/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     Flux<Event> events() {
         Event event1 = new Event(1L, "event1");
         Event event2 = new Event(2L, "event2");
         List<Event> list = Arrays.asList(event1, event2);
         return Flux.fromIterable(list);
+    }
+
+    @GetMapping(value = "/streams", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    Flux<Event> eventsStream() {
+        Stream stream = Stream.generate(() -> new Event(System.currentTimeMillis(),"value")).limit(10);
+        return Flux.fromStream(stream)
+            .delayElements(Duration.ofSeconds(1));
+        // 백그라운드 스레드에서 딜레이 처리 (해당 스레드에서 blocking)
+    }
+
+    @GetMapping(value = "/flux", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    Flux<Event> flux() {
+        Flux<Event> es = Flux
+            .<Event, Long>generate(() -> 1L, (id, sink) -> {
+                sink.next(new Event(id, "value"+ id));
+                return id+1;
+            })
+            .delayElements(Duration.ofSeconds(1))
+            .take(10);
+        Flux<Long> interval = Flux.interval(Duration.ofSeconds(1));
+        // Tuple로 만들어준다
+        // interval 데이터 딜레이가 있어서 기다리면서 tuple 생성도 딜레이가 생김
+        return Flux.zip(es, interval).map(t -> t.getT1());
     }
 
     public static void main(String[] args) {
