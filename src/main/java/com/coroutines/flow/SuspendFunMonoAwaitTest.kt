8071d22d183ce94575182e2001782e47de56ec36
Lauv <318921@naver.com>
2022-05-13 18:51:33 +0900

    mono x coroutines 실험

--- src/main/java/com/coroutines/flow/SuspendFunMonoAwaitTest.kt
+++ src/main/java/com/coroutines/flow/SuspendFunMonoAwaitTest.kt
@@ -1,18 +1,14 @@
 package com.coroutines.flow
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.reactor.awaitSingle
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
 import reactor.core.publisher.Mono
 import reactor.core.scheduler.Schedulers
+import kotlin.coroutines.coroutineContext
 
 fun main() {
     runBlocking {
-        println("runblocking: $coroutineContext")
+        println("runblocking: ${coroutineContext} ${Thread.currentThread()}")
         val result = testThreeSuspend()
         println("result: $result")
     }
@@ -39,10 +35,10 @@ suspend fun hey(): String {
  */
 
 suspend fun testOneSuspend(): String {
-    println("Test One Suspend Start")
+    println("Test One Suspend Start: $coroutineContext ${Thread.currentThread()}")
     return Mono.defer {
         val a = Mono.just("JUST MONO")
-        println("Test One MonoDefer Passed")
+        println("Test One MonoDefer Passed ${Thread.currentThread()}")
         a
     }
         .log()
@@ -50,10 +46,10 @@ suspend fun testOneSuspend(): String {
 }
 
 suspend fun testTwoSuspend(): String {
-    println("Test Two Suspend Start")
+    println("Test Two Suspend Start: $coroutineContext ${Thread.currentThread()}")
     return Mono.defer {
         val a = Mono.just("JUST MANO")
-        println("Test Two MonoDefer Passed")
+        println("Test Two MonoDefer Passed ${Thread.currentThread()}")
         a
     }
         .subscribeOn(Schedulers.boundedElastic())
@@ -61,14 +57,15 @@ suspend fun testTwoSuspend(): String {
 }
 
 suspend fun testThreeSuspend(): String {
+    println("Test Three Suspend Start: $coroutineContext ${Thread.currentThread()}")
     return Mono.defer {
         val a = Mono.just("JUST MANA")
-        println("Test Three MonoDefer Passed")
+        println("Test Three MonoDefer Passed ${Thread.currentThread()}")
         a
     }
         .subscribeOn(Schedulers.newSingle("Three First"))
         .doOnNext {
-            println("DO ON NEXT!")
+            println("DO ON NEXT! ${Thread.currentThread()}")
             it
         }
         .subscribeOn(Schedulers.newSingle("Three Second"))
@@ -76,9 +73,10 @@ suspend fun testThreeSuspend(): String {
 }
 
 suspend fun testFourSuspend(): String {
+    println("Test Four Suspend Start: $coroutineContext ${Thread.currentThread()}")
     return Mono.defer {
         val a = Mono.just("JUST HANA")
-        println("Test Four MonoDefer Passed")
+        println("Test Four MonoDefer Passed ${Thread.currentThread()}")
         a
     }.block()
 }
